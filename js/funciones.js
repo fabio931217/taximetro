773bb1ba73052859f017df3b2d62b88901d43144
fabio931217 <ingeniero.fabio.garcia@gmail.com>
2015-09-20 20:38:21 -0500

    cambio

--- js/funciones.js
+++ js/funciones.js
@@ -1,10 +1,19 @@
 var segundos =0;
 var minutos =0;
+var val =0;
+var valor=0;
+var unidad =0;
+
+var t= "";
+var t2= "";
+
 
 
  $(document).ready(function(e) {
 
   $("#btnuevo").hide();
+   $("#recibe").hide();
+  $("#cont2").hide();
 
    });
 
@@ -12,76 +21,52 @@ function nuevo_calculo ()
 {
   if (confirm("¿Desea realizar un nuevo calculo?")) {location.reload();};
 
-
 }
+
+
+
  function calcular()
     {
 
-
-  $.ajax({
-
-
-            url:"calcular.php",
-            type: "POST",
-            data:$('#formulario').serialize(),
-        
-         beforeSend: function()
-            {
-                 //alert("Calculando direccion...");
-            },
-            //una vez finalizado correctamente
-            success: function(data){
-//alert(data);
-              var r = jQuery.parseJSON(data);
-            if (r.error == true)
-            {
-                for (ind in r.bad_fields)
-                {
-                    $("#" + r.bad_fields[ind]).addClass("error");
-                }
-                alert(r.msg);
-                
-            } else
-            {
               // alert(r.msg);
-              $("#recibe").html(r.msg);
-              $("#cont2").html(r.tabla2);
+
+if ( $("#buscar").val() != "")
+{
+
+
+              $("#recibe").show();
+              $("#cont2").show();
               $("#btnverde").hide();
               $("#btnuevo").show();
               $("#v2").html("$"+" "+0);
               $("#u2").html(0);
                reiniciar_tiempo();
 
+  
+}
+else
+{
+  alert("Ingrese Direccion");
+}
 
-              
-            }
-             
-              },
-  })
-   return false;
+ 
 
 }
+
+
 
  function tarifa()
     {
 
-$.ajax({
-            url:"tarifa.php",
-            type: "POST",
-            data:$('#formulario').serialize(),
-            success: function(data){
-        //alert(data);
-             var r = jQuery.parseJSON(data);
-             $("#v2").html("$"+" "+r.valor);
-             $("#val").val(r.valor);
-             $("#u2").html(r.unidades);
+valor=valor + 20000 ;
+unidad = valor/20000;
+
+             $("#v2").html("$"+" "+valor);
+             $("#val").val(valor);
+             $("#u2").html(unidad);
              
                          
-            }
-  })
-   return false;
-
-}
+    }
 
 
 function tiempo () 
